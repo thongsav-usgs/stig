04c587d65878d3472627e0bb5610af88364c002d
Suftin, Ivan <isuftin@usgs.gov>
2014-12-19 20:57:23 -0600

    4.1.2 Disable Send Packet Redirects

--- recipes/proc_hard.rb
+++ recipes/proc_hard.rb
@@ -24,6 +24,18 @@ template "/etc/security/limits.conf" do
   mode 0644
 end
 
+# 4.1.1 Disable IP Forwarding
+# The net.ipv4.ip_forward flag is used to tell the server whether it can
+# forward packets or not. If the server is not to be used as a router, set the flag to 0.
+
+# 4.1.2 Disable Send Packet Redirects
+# ICMP Redirects are used to send routing information to other hosts. As a host itself
+# does not act as a router (in a host only configuration), there is no need to send redirects.
+#
+# An attacker could use a compromised host to send invalid ICMP redirects to other router
+# devices in an attempt to corrupt routing and have users access a system set up by the
+# attacker as opposed to a valid system.
+
 template "/etc/sysctl.conf" do
   source "sysctl.conf.erb"
   owner "root"
