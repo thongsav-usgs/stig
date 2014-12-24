c463164096692f10bcfb4d05074f865a416c348e
Suftin, Ivan <isuftin@usgs.gov>
2014-12-23 20:59:43 -0600

    Split up audits recipe into "audits" which runs audits and "audit_scripts" which creates audit scripts @ /root
    Wrote a few serverspec tests

--- test/integration/default/serverspec/aide_spec.rb
+++ test/integration/default/serverspec/aide_spec.rb
@@ -4,7 +4,14 @@ require 'serverspec'
 set :backend, :exec
 
 describe package('aide') do
-
   it { should be_installed }  
+end
 
+describe file('/var/lib/aide/aide.db.gz') do
+  it { should be_file }
+  it { should be_owned_by 'root' }
+end
+
+describe cron do
+  it { should have_entry '0 5 * * * /usr/sbin/aide --check' }
 end
