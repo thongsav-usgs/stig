08d4366efd3239aa3d6993315546f9469822344c
Ivan Suftin <isuftin@usgs.gov>
2015-04-14 19:46:14 -0500

    Forgot quotes around package names

--- recipes/tcp_wrappers.rb
+++ recipes/tcp_wrappers.rb
@@ -13,9 +13,9 @@
 # Redhat 4.5
 
 if %w{rhel fedora centos}.include?(node["platform"])
-	package tcp_wrappers
+	package "tcp_wrappers"
 end
 
 if %w{debian ubuntu}.include?(node["platform"])
-	package tcpd
+	package "tcpd"
 end
